309d7e18f30ccfcde6dfbc813b4459a58973de59
Filmboard <business@webitlab.com>
2020-05-17 20:48:59 +0530

    worked on admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,21 +1,22 @@
 <!doctype html>
 <html>
 <head>
-   @include('includes.admin.head')
+    @include('includes.admin.head')
 </head>
 <body class="hold-transition sidebar-mini">
-  <div class="wrapper">
-     @include('includes.admin.header')
-	   <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-	    @include('includes.admin.sidebar')
-	  </aside>
-	    <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-		   @yield('content')
-		</div>
-        <!-- /.content-wrapper -->
-	  @include('includes.admin.footer')
-  </div>
+<div class="wrapper">
+@include('includes.admin.header')
+<!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        @include('includes.admin.sidebar')
+    </aside>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+    @include('includes.admin.footer')
+</div>
+@yield('page_script')
 </body>
 </html>
